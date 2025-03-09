40d8c2048fa5f5acd1317a36a6e4867bb2799bbe
Akash-YS05 <akash.pan05@gmail.com>
2025-03-09 13:57:52 +0530

    testing mint

--- src/CoilCoin.sol
+++ src/CoilCoin.sol
@@ -1,21 +1,21 @@
 // SPDX-License-Identifier: Unlicense
 
+//ERC-20 - lets us create token on blockchain
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 pragma solidity ^0.8.13;
 
 contract CoilCoin is ERC20 {
-    uint public num;
+   address owner;
 
     constructor(uint _initialValue) ERC20("coil", "COIL") {
-        // 
+        //_mint comes from ERC20 parent class
+        _mint(msg.sender, _initialValue);
+        owner = msg.sender; 
     } 
 
-    function increment() public {
-        num++;
-    }
-
-    function decrement() public {
-        num--;
+    function mint(address to, uint256 amount) public {
+        require(msg.sender == owner);    
+        _mint(to, amount);
     }
 }
--- test/CoilCoin.t.sol
+++ test/CoilCoin.t.sol
@@ -37,4 +37,12 @@ contract TestContract is Test {
     function testSimple() public {
         assertEq(uint(2), uint(2), "ok");
     }
+    function testMint() public {
+        c.mint(address(this), 100);
+        assertEq(c.balanceOf(address(this)), 100, "ok");
+        assertEq(c.balanceOf(0x1535784469C951F4d4F85B582E6646421618cb7A), 0, "ok");
+
+        c.mint(0x1535784469C951F4d4F85B582E6646421618cb7A, 100);
+        assertEq(c.balanceOf(0x1535784469C951F4d4F85B582E6646421618cb7A), 100, "ok");
+    }
 }
